b0ee01b1c940879ed6297ada29ff940d533c03b3
Brittany-Maraneta <Brittany-june@hotmail.com>
2024-03-10 16:18:16 -0600

    first commit

---
+++ activity4/src/App.vue
@@ -0,0 +1,116 @@
+<script setup>
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+import Contact from "./components/Contact.vue";
+import { ref } from "vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/contact", component: Contact },
+];
+
+const heroMessage = "Hello, welcome to my page!";
+const isVisible = ref(true);
+
+const handleClickAndToggleVisibility = () => {
+  handleClick();
+  toggleVisibility();
+};
+
+const handleClick = () => {
+  console.log("Button clicked!");
+};
+
+const toggleVisibility = () => {
+  isVisible.value = !isVisible.value;
+};
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <router-link to="#">Home</router-link>
+      <router-link to="/components/About.vue">About</router-link>
+      <router-link to="/components/Contact.vue">Contact</router-link>
+    </ul>
+  </nav>
+
+  <section class="header">
+    <h1>{{ heroMessage }}</h1>
+
+    <div class="button">
+      <button class="button2" @click="handleClickAndToggleVisibility">
+        Click Me
+      </button>
+
+      <p v-if="isVisible">This is a Vanilla page I edited using Vue!</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  margin-left: auto;
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+}
+
+nav ul router-link {
+  margin-right: 15px;
+  margin-left: 15px;
+}
+
+nav ul router-link a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.header {
+  background-image: url("https://images.pexels.com/photos/414659/pexels-photo-414659.jpeg");
+  background-size: cover;
+  background-position: center;
+  height: 500px;
+  width: 140%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  text-align: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.button2 {
+  justify-content: center;
+  margin-top: 10px;
+  padding: 30px, 30px;
+  background-color: #333;
+  color: white;
+}
+
+.button button:hover {
+  cursor: pointer;
+  background-color: lightgrey;
+  color: black;
+}
+</style>
---
+++ activity4/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<!-- Footer.vue -->
+<template>
+  <footer class="footer">
+    <div class="footer-content">
+      <h3>Hello</h3>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+// Footer component logic, if any, goes here
+</script>
+
+<style scoped>
+/* Footer component styles go here */
+.footer {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+}
+</style>
